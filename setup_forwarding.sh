6580d814d09adec185b806a3e47f51ca6c657e00
sharadg <91043+sharadg@users.noreply.github.com>
2019-08-14 03:01:07 -0500

    commenting off ip_forward flag by default

--- setup_forwarding.sh
+++ setup_forwarding.sh
@@ -1,5 +1,5 @@
 
-sysctl -w net.ipv4.ip_forward=1
+#sysctl -w net.ipv4.ip_forward=1
 iptables -A FORWARD -s 10.240.0.0/16 -j ACCEPT
 iptables -A FORWARD -d 10.240.0.0/16 -j ACCEPT
 iptables -t nat -A POSTROUTING -s 10.240.0.0/24 ! -o cni0 -j MASQUERADE
